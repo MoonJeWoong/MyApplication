2c08beff3f9c1ac73e7235c7c426614471a71a9f
jisung0920 <jisung0920@gmail.com>
2017-09-18 20:15:11 +0900

    Initial commit

---
+++ app/src/main/java/com/example/jisung/myapplication/Main2Activity.java
@@ -0,0 +1,31 @@
+package com.example.jisung.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Main2Activity extends AppCompatActivity {
+
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        webView = (WebView)findViewById(R.id.web);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setSupportZoom(true);
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("http://www.google.com");
+
+    }
+}
---
+++ app/src/main/java/com/example/jisung/myapplication/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.jisung.myapplication;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView t1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        t1 = (TextView)findViewById(R.id.text);
+        registerForContextMenu(t1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu1,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.m1:
+                t1.setTextColor(Color.RED);
+                Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.m2:
+                Intent intent = new Intent(this,Main2Activity.class);
+                startActivity(intent);
+
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        return super.onContextItemSelected(item);
+
+
+    }
+}
